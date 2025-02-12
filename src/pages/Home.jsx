3e176a3443655f5ee0ab6d71466b2eba991d659e
hxrii <harikrishnankannoth@gmail.com>
2025-02-12 22:55:40 +0530

    SearchBar added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,22 @@ function Home(){
         {id:7, title:"Pulp Fiction", image:"www.google.com",description:"Pulp Fiction"},
     ]
 
+    const handleSearch = () => {
+
+    }
 
     return (
-        <div>
+        <div className="home">
             <h1>Movies</h1>
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Search movies..." className="search-input" />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+            <div className="movie-grid">
             {movies.map((movie) => (
                 <MovieCard key={movie.id} movie={movie} />
             ))}
+            </div>
         </div>
     )
 }
