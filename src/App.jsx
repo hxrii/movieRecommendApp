3e59c27b90f25801a369072b5e81f43de04d19c8
hxrii <harikrishnankannoth@gmail.com>
2025-02-15 10:53:26 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from'react-router-dom'
 import Home from './pages/Home'
+import Favourites from './pages/Favourites'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
-      <Home/>
+   
+    <Router>
+    <Navbar></Navbar>
+      
+    <main className='main-content'>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/favourites" element={<Favourites/>}></Route>
+        <Route path="*" element={<h1>Page Not Found</h1>}></Route>
+
+      </Routes>
+      
+    </main>  
+    </Router>
     </>
   )
 }
